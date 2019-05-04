ad0dd1aafdd7b31f05c5c949c525ae8c04e05678
Dominick Geronimo <lorddom22@gmail.com>
2019-05-04 12:39:11 -0400

    database is up and running, still need to import data

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    // Giving the User model a name of type STRING
     userId: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
