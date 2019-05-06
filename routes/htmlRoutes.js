856a2c9cbef4cc816600a1fd1bba1490b73f1170
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-05-06 14:37:04 -0400

    footer

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Hike.findAll({}).then(function(hike) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: Hike
       });
     });
   });
