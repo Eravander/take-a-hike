cf9fdaee33c97b3997a7f7c2ee243ac064632e4f
Madelyn Y <maddy13vb@gmail.com>
2019-05-06 18:06:16 -0400

    assets folder added

--- public/dummy-data/dummy-data.js
+++ public/dummy-data/dummy-data.js
@@ -1,3 +1,19 @@
-$("#menu").hover(function () {
-    $('body').toggleClass("active");
+function clickSet() {
+    circularnav.classList.toggle("closed")
+    circularnav.classList.toggle("clicked");
+    if (circularnav.classList.contains("closed")) {
+        // freshly closed button
+        var pseudoBefore = window.getComputedStyle(
+            document.querySelector('.ss-icon'), ':before'
+        ).animation
+    }
+}
+
+var circularnav = document.getElementsByClassName("ss-icon")[0];
+circularnav.addEventListener("click", clickSet, false);
+
+circularnav.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+        clickSet();
+    }
 });
